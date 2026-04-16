31e285db5fe92f5af5538fdd2be4831eac5619d4
danlins128 <danlins8049@gmail.com>
2026-04-16 01:40:08 -0300

    Todas as funções lógicas funcionando. hora de criar o banco de dados e as funções de login

--- classes_db.py
+++ classes_db.py
@@ -41,6 +41,25 @@ class ContaService:
 		cliente.saldo -= valor
 		print(f"Saque de {valor:,.2f} efetuado com sucesso\n")
 		
+	def transferir(self, origem_login, destino_login, valor):
+	  cliente_origem = self.banco.buscar_usuario(origem_login)
+	  cliente_destino = self.banco.buscar_usuario(destino_login)
+	  if cliente_origem is None or cliente_destino is None:
+	    print("Usuário não existe ou não encontrado")
+	    return
+	  if valor <= 0:
+	    print("Valor incorreto.")
+	    return 
+	  if cliente_origem.saldo < valor:
+	    print("Saldo insuficiente para a transaçao")
+	    return
+	  cliente_origem.saldo -= valor
+	  cliente_destino.saldo += valor
+	  print(f"Transferência de {cliente_origem.nome} para {cliente_destino.nome}
+	  realizado com sucesso")
+	  print(f"Novo saldo atual de {cliente_origem.nome}, R$ {cliente_origem.saldo}.")
+	  print(f"Novo saldo atual de {cliente_destino.nome} R$ {cliente_destino.saldo}.")
+		  
 class Usuarios:
 	def __init__(self, nome, login, saldo):
 		self.nome=nome
@@ -69,9 +88,12 @@ else:
 service=ContaService(banco)
 service.depositar("nyele128", 400)
 
-u.exibir()
+#u.exibir()
 
 a=banco.buscar_usuario("dan123")
 service.sacar("dan123", 100)
 
-a.exibir()
+#a.exibir()
+
+service.transferir("nyele128", "dan123", 500)
+#a.exibir()
