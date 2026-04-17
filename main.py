62c86c4a260ef8a35d82190e2863e8bf1e287c70
danlins128 <danlins8049@gmail.com>
2026-04-17 15:34:25 -0300

    Lógica pronta para push

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ service = ContaService(repo)
 
 #service.cadastrar("Daniel", "danlins", "dan123")
 #service.cadastrar("Daniele", "nyele", "ny123")
-
+#service.cadastrar("Daniel Danas", "dantasalves", "dantas123")
+#service.login("danlins", "dan123")
